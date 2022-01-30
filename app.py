d424b44bede6e65f8504a399312fa692278b6c11
Howard Tsai <howard0505011@gmail.com>
2022-01-30 23:16:01 +0800

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, ImageSendMessage
 )
 
 app = Flask(__name__)
@@ -38,7 +38,48 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '親一個老公~~'
+    s = '今天晚上特別允許你可以...嗯..對我亂來喔~~(臉紅)'
+    
+    if '圖片' in msg:
+        image_message = ImageSendMessage(
+            original_content_url='https://i.imgur.com/ErRXu3m.jpg',
+            preview_image_url='https://i.imgur.com/ErRXu3m.jpg'
+        )
+    
+        line_bot_api.reply_message(
+            event.reply_token,
+            image_message)
+        return
+
+    if '貼圖' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='2',
+            sticker_id='36'
+        )
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            sticker_message)
+        return
+
+
+    if msg in ['我愛你', '我愛妳']:
+        s = '啊.啊..不可以色色啦!老公 >.< ||  我也愛你~~love you too'
+    elif msg == '親一個':
+        s = '討厭啦~~ 我同意 但是要把我抱起來親喔!!皓宇寶貝'
+    elif msg == '愛妳寶貝':
+        s = '嘴巴真甜 真是的~~拿你沒辦法(害羞)/// 今晚同意你做...(色色' 
+    elif '老婆' in msg:
+        s = '記得回家要親一個喔~~ by洛琪希' 
+    elif '頭髮' in msg:
+        s = '等等洗完再讓你聞個夠啦! 小色鬼~'  
+    elif '累' in msg:
+        s = '辛苦你了 晚上回家給你小獎勵吧! 膝枕或..那個..都可以喔!'
+    elif msg in ['洗澡', '一起', '疲累']:
+        s = '那..今晚要不要陪我一起洗澡呢?(臉紅) 看你平常對我很貼心~ 今天允許你洗澡時可以亂來呦!'
+    elif msg == '今晚我想吃掉你':
+        s = '那要對我溫柔一點喔..啾咪~~'
+
     
     line_bot_api.reply_message(
         event.reply_token,
